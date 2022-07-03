6f3018915a5e724f3df24444adb5e043376f703b
DaOfficialWizard <pyr0ndet0s97@gmail.com>
2022-07-03 15:02:27 +0100

    added windows example

--- .virtualenvs/example.py
+++ .virtualenvs/example.py
@@ -7,10 +7,19 @@ if __name__ == '__main__':
     autoconnect.begin()
     print(autoconnect.serial)
     
-    # Manual connect
+    # Manual connect linux
     # first argument is the port
     # second argument is the force flag
     autoconnect = AutoDetectSerialPort("dev/ttyUSB0", 1)
+    autoconnect.begin()
+    
+    # Access the Serial object
+    print(autoconnect.serial)
+    
+    # Manual connect Windows
+    # first argument is the port
+    # second argument is the force flag
+    autoconnect = AutoDetectSerialPort("COM5", 1)
     autoconnect.begin()
     
     # Access the Serial object
